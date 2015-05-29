c453829a339417aa975013a1735fd9bdff9b346b
nxbtch <nxbtch@163.com>
2015-05-30 01:19:00 +0800

    echo line

--- install_rvm_with_stable.sh
+++ install_rvm_with_stable.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+
 sudo apt-get install curl
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-source ~/.profile
+
+command_exists(){
+    type "$1" &> /dev/null
+}
+
+echo_line(){
+    echo "******************"
+    echo $1
+    echo "******************"
+}
+
+if command_exists rvm; then
+    echo_line "rvm installed"
+else
+    echo_line "rvm installing"
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+    curl -sSL https://get.rvm.io | bash -s stable
+    source ~/.rvm/scripts/rvm
+    source ~/.profile
+    echo_line "rvm installed"
+fi
+
 rvm install ruby-2.2
+rvm use ruby-2.2 --default
