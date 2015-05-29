7117f85e486d77644720d15bb859b8941b196592
nxbtch <nxbtch@163.com>
2015-05-30 01:12:21 +0800

    default install 2.2

--- install_rvm_with_stable.sh
+++ install_rvm_with_stable.sh
@@ -5,4 +5,4 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 source ~/.profile
-rvm install ruby-head
+rvm install ruby-2.2
