4ae3c5ec75051ba7d35270b49dbe174911b2a5b5
nxbtch <hechen1995@gmail.com>
2015-08-02 01:25:06 +0800

    change file name

--- yiqi.sh
+++ yiqi.sh
@@ -2,30 +2,30 @@
 
 S="ubuntu@54.187.78.39"
 
-function yiqi {
+function server {
     ssh -t $S "$1"
 }
 
 if [ $# -eq 0 ]
 then
-    yiqi
+    server
     exit
 fi
 
 case "$1" in
     install)
-        yiqi "sudo apt-get install $2"
+        server "sudo apt-get install $2"
         ;;
     update)
-        yiqi "sudo apt-get update"
+        server "sudo apt-get update"
         ;;
     dl)
-        yiqi "rm /home/ubuntu/a.mp4 && youtube-dl $2 -o /home/ubuntu/a.mp4"
+        server "rm /home/ubuntu/a.mp4 && youtube-dl $2 -o /home/ubuntu/a.mp4"
         scp $S:/home/ubuntu/a.mp4 ~/Downloads/a.mp4
         open ~/Downloads/a.mp4
         ;;
     log)
-        yiqi "tail -f /var/log/shadowsocks.log"
+        server "tail -f /var/log/shadowsocks.log"
         ;;
     *)
         echo "not support option '$1' yet"
